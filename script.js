dc0204e5f54c3441e88fb0289d787f49f6b96fd2
Oyimhon <97nizamova97@gmail.com>
2021-12-02 17:07:38 +0500

    changes

---
+++ script.js
@@ -0,0 +1,23 @@
+/*Вызов плагина */
+$(document).ready(function(){
+    $("#slider").owlCarousel();
+  });
+
+$(document).ready(function(){
+    const slider = $("#slider").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+});
